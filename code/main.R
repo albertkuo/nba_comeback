0721636d3fda5e7a0fb1b3d67475ef4ead37221c
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2020-11-01 15:54:34 -0500

    Change messages

--- code/main.R
+++ code/main.R
@@ -18,17 +18,17 @@ game_ids_regular = game_ids[[1]]
 game_ids_playoffs = game_ids[[2]]
 game_ids_all = c(game_ids_regular, game_ids_playoffs)
 
-## Get game IDs previously scraped already
+## Get game IDs previously downloaded already
 if(file.exists(here("./data/game_ids.txt"))){
   game_ids_previous = readLines(here("./data/game_ids.txt"))
 } else {
   game_ids_previous = c()
 }
 
-## Only scrape new game IDs
+## Only download new game IDs
 game_ids_regular = setdiff(game_ids_regular, game_ids_previous)
 game_ids_playoffs = setdiff(game_ids_playoffs, game_ids_previous)
-message("Scraping ", length(game_ids_regular), " regular season games and ",
+message("Downloading ", length(game_ids_regular), " regular season games and ",
         length(game_ids_playoffs), " playoff games")
 
 ## Get play by play for every game ID
@@ -37,7 +37,7 @@ empty_games = 0
 games_regular = vector(mode = "list", length = length(game_ids_regular))
 for(i in seq_along(game_ids_regular[1:100])){
   if(i %% 10 == 0){
-    message("Scraped ", i, " out of ", length(game_ids_regular), " games...")
+    message("Downloaded ", i, " out of ", length(game_ids_regular), " games...")
   }
 
   game_id = game_ids_regular[[i]]
@@ -48,7 +48,7 @@ for(i in seq_along(game_ids_regular[1:100])){
     empty_games = empty_games + 1
 }
 
-message("Failed to scrape ", empty_games, " out of ", length(game_ids_regular), " regular season games")
+message("Failed to download ", empty_games, " out of ", length(game_ids_regular), " regular season games")
 
 
 
