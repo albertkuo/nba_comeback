cce76e9ce688e22b14207de91f69894295513e92
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2020-11-01 14:14:50 -0500

    Save game IDs

--- code/main.R
+++ code/main.R
@@ -3,7 +3,7 @@
 # Author:             Albert Kuo
 # Date last modified: Oct 30, 2020
 #
-# Main scripts for analysis
+# Main script for analysis
 
 
 # Packages
@@ -12,10 +12,30 @@ library(here)
 source_python(here("./code/download_data.py")) # Call Python script to use NBA API
 
 # Download data
-game_ids = get_game_ids()
-game_ids_regular = game_ids[1]
-game_ids_playoffs = game_ids[2]
+## Get game IDs
+game_ids = get_game_ids() # function in download_data.py
+game_ids_regular = game_ids[[1]]
+game_ids_playoffs = game_ids[[2]]
+game_ids_all = c(game_ids_regular, game_ids_playoffs)
 
-# If there are new game IDs, download their data
-# download_ids()
-# download_playbyplay(game_ids)
+## Get game IDs previously scraped already
+if(file.exists(here("./data/game_ids.txt"))){
+  game_ids_previous = readLines(here("./data/game_ids.txt"))
+} else {
+  game_ids_previous = c()
+}
+
+## Only scrape new game IDs
+game_ids_regular = setdiff(game_ids_regular, game_ids_previous)
+game_ids_playoffs = setdiff(game_ids_playoffs, game_ids_previous)
+message("Scraping ", length(game_ids_regular), " regular season games and ",
+        length(game_ids_playoffs), " playoff games.")
+
+## Get play by play for every game ID
+get_play_by_play(game_id)
+
+
+# Save game IDs that have been scraped
+game_ids_scraped = c(game_ids_previous, game_ids_regular, game_ids_playoffs)
+write(game_ids_scraped, here("./data/game_ids.txt"))
+
