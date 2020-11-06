f08ad006b649ee4f6b4d5d460de8addc7f99f183
Albert Kuo <9829831+albertkuo@users.noreply.github.com>
2020-11-05 23:20:30 -0500

    Change y scale to -30 to +30

--- code/plot_data.R
+++ code/plot_data.R
@@ -13,10 +13,10 @@ plot_data = function(df){
     facet_grid(. ~ quarter, scales = "free_x", space = "free_x") +
     scale_x_continuous(breaks = c(seq(-12, 0, by = 2)),
                        labels = c(seq(12, 0, by = -2))) +
-    scale_y_continuous(breaks = seq(-40, 40, by = 5),
-                       labels = c(seq(-40, 0, by = 5),
-                                  paste0("+", seq(5, 40, by = 5))),
-                       limits = c(-40, 40)) +
+    scale_y_continuous(breaks = seq(-30, 30, by = 5),
+                       labels = c(seq(-30, 0, by = 5),
+                                  paste0("+", seq(5, 30, by = 5))),
+                       limits = c(-30, 30)) +
     scale_fill_gradient2(midpoint = 0.5,
                          low = "#d63a3a", mid = "#ffd700", high = "#33aa00") +
     labs(subtitle = "Quarters",
